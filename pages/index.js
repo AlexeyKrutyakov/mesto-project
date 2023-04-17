3ff72eb3d0444c8bf598bb3175b52546bafc13bc
Alexey Krutyakov <coder.arctik@gmail.com>
2023-04-17 23:22:09 +0300

    fix: delete old functions

--- pages/index.js
+++ pages/index.js
@@ -4,67 +4,86 @@ const profile = {
   text: "Исследователь океана",
 };
 
-const places = {
-  "Чагаз-Узун": {
-    src: "./images/mars-mountains.jpg",
+const initialCards = [
+  {
+    name: "Чагаз-Узун",
+    link: "./images/mars-mountains.jpg",
     alt: "Долина с рыжыми пейзажами на фоне голубых гор",
   },
-  Рускеала: {
-    src: "./images/ruskeala-failure.jpg",
+  {
+    name: "Рускеала",
+    link: "./images/ruskeala-failure.jpg",
     alt: "Провал в пещеру",
   },
-  Магадан: {
-    src: "./images/magadan.jpg",
+  {
+    name: "Магадан",
+    link: "./images/magadan.jpg",
     alt: "Закат на море в Магадане",
   },
-  Якутск: {
-    src: "./images/yakutsk.jpg",
+  {
+    name: "Якутск",
+    link: "./images/yakutsk.jpg",
     alt: "Лошади в снегу",
   },
-  "Петропавловск Камчатский": {
-    src: "./images/petropavlovsk-kamchatskiy.jpg",
+  {
+    name: "Петропавловск Камчатский",
+    link: "./images/petropavlovsk-kamchatskiy.jpg",
     alt: "Город у подножия вулкана",
   },
-  Молога: {
-    src: "./images/flooded-city.jpg",
+  {
+    name: "Молога",
+    link: "./images/flooded-city.jpg",
     alt: "Затопленный монастырь в озере",
   },
-};
+];
 
 // define class for opened popup
 const openedPopupClass = "popup_opened";
 
+// define class for 'close popup' buttons
+const btnClosePopupClass = "popup__close-button";
+
 // define 'edit profile' button
-const editProfileBtnClass = "profile__edit-button";
-const editProfileBtn = document.querySelector(`.${editProfileBtnClass}`);
+const btnEditProfileClass = "profile__edit-button";
+const btnEditProfile = document.querySelector(`.${btnEditProfileClass}`);
 
 // define 'edit profile' popup
-const editProfilePopupClass = "popup_type_edit-profile";
-const editProfilePopup = document.querySelector(`.${editProfilePopupClass}`);
+const popupEditProfileClass = "popup_type_edit-profile";
+const popupEditProfile = document.querySelector(`.${popupEditProfileClass}`);
+// define form for edit profile
+const formEditProfile = popupEditProfile.querySelector(".form");
+
+// define button 'close edit-profile popup'
+const btnClosePopupEditProfile = popupEditProfile.querySelector(
+  `.${btnClosePopupClass}`
+);
+
+// define button 'submit form edit profile'
+const btnSubmitFormEditProfile =
+  formEditProfile.querySelector(".form__save-button");
 
 // define 'add place' button
 const addPlaceBtnClass = "profile__add-button";
 const addPlaceBtn = document.querySelector(`.${addPlaceBtnClass}`);
 
 // define 'add place' popup
-const addPlacePopupClass = "popup_type_add-place";
-const addPlacePopup = document.querySelector(`.${addPlacePopupClass}`);
+const popupAddPlaceClass = "popup_type_add-place";
+const popupAddPlace = document.querySelector(`.${popupAddPlaceClass}`);
+
+// define form add place
+const formAddPlace = popupAddPlace.querySelector(".form");
+
+// define button 'close add place popup'
+const btnClosePopupAddPlace = popupAddPlace.querySelector(
+  `.${btnClosePopupClass}`
+);
+
+// define button 'submit' form add new place
+const btnSubmitFormAddPlace = popupAddPlace.querySelector(".form__save-button");
 
 // define 'enlarge image' popup
 const imagePopupClass = "popup_type_enlarge-image";
 const imagePopup = document.querySelector(`.${imagePopupClass}`);
-
-// define list of buttons and the popups they open
-const popups = {
-  editProfile: {
-    btn: editProfileBtn,
-    popup: editProfilePopup,
-  },
-  addPlace: {
-    btn: addPlaceBtn,
-    popup: addPlacePopup,
-  },
-};
 
 // define section cards
 const cards = document.querySelector(".cards");
@@ -88,8 +107,8 @@ const likeBtnClass = "card__like-button";
 const activeLikeBtnClass = "card__like-button_active";
 
 // define profile name and profile text elements
-let profileNameElement = document.querySelector(".profile__name");
-let profileTextElement = document.querySelector(".profile__text");
+const profileNameElement = document.querySelector(".profile__name");
+const profileTextElement = document.querySelector(".profile__text");
 
 // define 'profile name' input value
 const inputProfileName = document.querySelector(
@@ -108,96 +127,71 @@ const inputPlaceImage = document.querySelector(
 );
 
 // define card template
-const cardTemplate = document.querySelector("#card-template").content;
+const cardTemplate = document.querySelector("#card-template");
 
-function definePopupBtn(popup, btnClass) {
-  return popup.querySelector(`.${btnClass}`);
-}
-
-function openPopup(target) {
-  let popup;
-  let saveFormBtn;
-
-  if (target.classList.value.split(" ").includes(cardImageClass)) {
-    popup = imagePopup;
-    const image = popup.querySelector(".popup__image");
-    image.src = target.src;
-    const name = target.parentElement.querySelector(".card__name").textContent;
-    const figcaption = popup.querySelector(".popup__figcaption");
-    figcaption.textContent = name;
-  } else {
-    switch (target) {
-      case editProfileBtn:
-        popup = popups.editProfile.popup;
-        // initiate input values with current profile data
-        inputProfileName.value = profile.name;
-        inputProfileText.value = profile.text;
-        break;
-      case addPlaceBtn:
-        popup = popups.addPlace.popup;
-        inputPlaceName.value = "";
-        inputPlaceImage.value = "";
-        break;
-    }
-    // define save button for opened popup
-    saveFormBtn = definePopupBtn(popup, saveFormBtnClass);
-    saveFormBtn.addEventListener("click", submitForm);
-  }
-  // add open modifier to popup
+function openPopup(popup) {
   popup.classList.add(openedPopupClass);
-
-  // define close button for opened popup
-  const popupCloseBtn = definePopupBtn(popup, popupCloseBtnClass);
-
-  // add listeners to close button and save button for opened popup
-  popupCloseBtn.addEventListener("click", closePopup);
 }
 
-function closePopup(event) {
-  // define popup to close
-  const clickedBtn = event.target;
-  const popup = parentOfClickedBtn(clickedBtn);
-  // add close modifier to popup
+function openPopupEditProfile() {
+  openPopup(popupEditProfile);
+  // initiate input values with current profile data
+  inputProfileName.value = profile.name;
+  inputProfileText.value = profile.text;
+  // add listeners for popup buttons
+  btnClosePopupEditProfile.addEventListener("click", () =>
+    closePopup(popupEditProfile)
+  );
+  btnSubmitFormEditProfile.addEventListener("click", (e) =>
+    submitFormEditProfile(e, formEditProfile)
+  );
+}
+
+function openPopupAddPlace() {
+  openPopup(popupAddPlace);
+  console.log(popupAddPlace);
+  // clear old name and text in input fields
+  inputPlaceName.value = "";
+  inputPlaceImage.value = "";
+  // add listeners for popup buttons
+  btnClosePopupAddPlace.addEventListener("click", () =>
+    closePopup(popupAddPlace)
+  );
+  btnSubmitFormAddPlace.addEventListener("click", (e) =>
+    submitFormAddPlace(e, formAddPlace)
+  );
+}
+
+function closePopup(popup) {
   popup.classList.remove(openedPopupClass);
 }
 
-function submitForm(event) {
-  // override the default submit behavior
+function submitFormEditProfile(event, form) {
+  // undo standard sumbit behavior
   event.preventDefault();
-
-  // define opened popup
-  const clickedBtn = event.target;
-  const popup = parentOfClickedBtn(clickedBtn);
-
-  // define classes of opened popup
-  const popupClasses = popup.classList.value.split(" ");
-
-  // read data from form
-  const inputData = popup.querySelectorAll(".form__input");
-
-  // save profile if popup is profile form
-  if (popupClasses.includes(editProfilePopupClass)) {
-    saveProfile(inputData);
-  }
-
-  // add place if popup is place form
-  if (popupClasses.includes(addPlacePopupClass)) {
-    addPlace(inputData);
-  }
-
-  // refresh profile data into the site
+  // extract info from input
+  const data = extractDataFromForm(form);
+  // update profile
+  saveNewProfile(data);
   refreshProfile();
 
-  // close opened popup
-  popup.classList.remove(openedPopupClass);
+  closePopup(popupEditProfile);
 }
 
-function saveProfile(inputData) {
+function submitFormAddPlace(event, form) {
+  // undo standard sumbit behavior
+  event.preventDefault();
   // extract info from input
-  const data = extractDataFromInput(inputData);
+  const data = extractDataFromForm(form);
+  // add new place
+  addNewPlace(data);
+
+  closePopup(popupAddPlace);
+}
+
+function saveNewProfile(data) {
   // get keys of data object
   const keys = Object.keys(data);
-
   // update profile object
   keys.forEach((key) => {
     switch (key) {
@@ -216,26 +210,12 @@ function refreshProfile() {
   profileTextElement.textContent = profile.text;
 }
 
-function addPlace(inputData) {
-  const data = extractDataFromInput(inputData);
-  createPlace(cardTemplate, data["place-name"], data["place-image"]);
-}
-
-function parentOfClickedBtn(btn) {
-  let parent = btn.parentElement;
-  let parentClasses = parent.classList.value.split(" ");
-  while (!parentClasses.includes("popup") && !parentClasses.includes("card")) {
-    parent = parent.parentElement;
-    parentClasses = parent.classList.value.split(" ");
-  }
-  return parent;
-}
-
-function extractDataFromInput(inputData) {
+function extractDataFromForm(form) {
   const data = {};
-  for (let i = 0; i < inputData.length; i++) {
-    const name = inputData[i].name;
-    const value = inputData[i].value;
+  const inputList = form.querySelectorAll(".form__input");
+  for (let i = 0; i < inputList.length; i++) {
+    const name = inputList[i].name;
+    const value = inputList[i].value;
     data[name] = value;
   }
   return data;
@@ -250,59 +230,15 @@ function toggleLike(likeBtn) {
   }
 }
 
-function eventRunner(event) {
-  switch (event.target) {
-    case editProfileBtn:
-      openPopup(editProfileBtn);
-      break;
-    case addPlaceBtn:
-      openPopup(addPlaceBtn);
-      break;
-  }
-
-  const targetClasses = event.target.classList.value.split(" ");
-  if (targetClasses.includes(likeBtnClass)) {
-    toggleLike(event.target);
-  }
-  if (targetClasses.includes(removeCardBtnClass)) {
-    removePlace(event.target);
-  }
-  if (targetClasses.includes(cardImageClass)) {
-    openPopup(event.target);
-  }
-}
-
-function createPlace(cardTemplate, placeName, placeImage, imageAlt = "") {
-  // create copy of template
-  const template = cardTemplate.cloneNode(true);
-  // define and initialize card name
-  const cardName = template.querySelector(".card__name");
-  cardName.textContent = placeName;
-  // define and initialize card image
-  const cardImage = template.querySelector(".card__image");
-  cardImage.src = placeImage;
-  cardImage.alt = imageAlt;
-  // add card into the cards
-  cards.prepend(template);
-}
-
-function removePlace(removeBtn) {
-  const card = parentOfClickedBtn(removeBtn);
-  card.remove();
-}
-
 // initialize profile data
 refreshProfile();
 
 // initialize place cards
-const placesKeys = Object.keys(places);
-placesKeys.forEach((key) => {
-  createPlace(cardTemplate, key, places[key].src, places[key].alt);
-});
 
-// initialize listener for document
-document.addEventListener("click", (e) => eventRunner(e));
+btnEditProfile.addEventListener("click", openPopupEditProfile);
+addPlaceBtn.addEventListener("click", openPopupAddPlace);
 
 // TO-DO:
 // 2. fix: github pages
 // 3. fix: big image width < 320px -> crash
+// - openPopupEditProfile <- refactor with fillForm()
