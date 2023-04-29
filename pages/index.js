de588fb59bdfca2b3a493774f8b96ccb8783bf61
Alexey Krutyakov <coder.arctik@gmail.com>
2023-04-29 16:46:56 +0300

    refactoring

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,70 @@
+// page
+const page = document.querySelector('.page');
+// PROFILE SECTION
+const profileSection = page.querySelector('.profile');
+// profile name and profile text elements
+const elementProfileName = page.querySelector('.profile__name');
+const elementProfileText = page.querySelector('.profile__text');
+// button for edit profile
+const classBtnEditProfile = 'profile__edit-button';
+const btnEditProfile = profileSection.querySelector(`.${classBtnEditProfile}`);
+// 'add place' button
+const classBtnAddPlace = 'profile__add-button';
+const btnAddPlace = profileSection.querySelector(`.${classBtnAddPlace}`);
+// CARDS SECTION
+const cardsSection = page.querySelector('.cards');
+// card template
+const templateCard = page.querySelector('#card-template').content;
+// card image class
+const classCardImage = 'card__image';
+// like button class
+const classLikeBtn = 'card__like-button';
+// define class for active 'like' button
+const classActiveLikeBtn = 'card__like-button_active';
+// remove card button class
+const classRemoveCardBtn = 'card__remove-button';
 // POPUPS
 // class for opened popup
-const classOpenedPopup = "popup_opened";
+const classOpenedPopup = 'popup_opened';
 // popup for edit profile
-const popupEditProfile = document.querySelector(".popup_type_edit-profile");
-// popup for add place
-const popupAddPlace = document.querySelector(".popup_type_add-place");
-// popup with 'enlarge image place'
-const popupEnlargeImage = document.querySelector(".popup_type_enlarge-image");
-// elements of enlarge image popup
-const popupImage = popupEnlargeImage.querySelector(".popup__image");
-const popupFigcaption = popupEnlargeImage.querySelector(".popup__figcaption");
-
-// FORMS
-// form for edit profile
-const formEditProfile = popupEditProfile.querySelector(".form");
-// form for add place
-const formAddPlace = popupAddPlace.querySelector(".form");
-// 'profile name' input value
-const inputProfileName = document.querySelector(
-  ".form__input[name=profile-name]"
-);
-// 'profile text' input value
-const inputProfileText = document.querySelector(
-  ".form__input[name=profile-text]"
-);
-// define 'place name' input value
-const inputPlaceName = formAddPlace.querySelector(
-  ".form__input[name=place-name]"
-);
-// define 'place text' input value
-const inputPlaceImage = formAddPlace.querySelector(
-  ".form__input[name=place-image]"
-);
-
-// BUTTONS
-// define class for active 'like' button
-const classActiveLikeBtn = "card__like-button_active";
-// button for edit profile
-const btnEditProfile = document.querySelector(".profile__edit-button");
-// 'add place' button
-const btnAddPlace = document.querySelector(".profile__add-button");
+const popupEditProfile = page.querySelector('.popup_type_edit-profile');
 // button 'close edit-profile popup'
 const btnClosePopupEditProfile = popupEditProfile.querySelector(
-  ".popup__close-button"
+  '.popup__close-button'
 );
+// popup for add place
+const popupAddPlace = page.querySelector('.popup_type_add-place');
+// popup with 'enlarge image place'
+const popupEnlargeImage = page.querySelector('.popup_type_enlarge-image');
+// elements of enlarge image popup
+const popupImage = popupEnlargeImage.querySelector('.popup__image');
+const popupFigcaption = popupEnlargeImage.querySelector('.popup__figcaption');
 // button close 'add place popup'
 const btnClosePopupAddPlace = popupAddPlace.querySelector(
-  ".popup__close-button"
+  '.popup__close-button'
 );
 // button close 'enlarge image popup'
 const btnClosePopupEnlargeImage = popupEnlargeImage.querySelector(
-  ".popup__close-button"
+  '.popup__close-button'
 );
 
-// profile name and profile text elements
-const elementProfileName = document.querySelector(".profile__name");
-const elementProfileText = document.querySelector(".profile__text");
-// section cards
-const cards = document.querySelector(".cards");
-// card template
-const templateCard = document.querySelector("#card-template").content;
+// FORMS
+// form for edit profile
+const formEditProfile = popupEditProfile.querySelector('.form');
+// form for add place
+const formAddPlace = popupAddPlace.querySelector('.form');
+// 'profile name' input value
+const inputProfileName = page.querySelector('.form__input[name=profile-name]');
+// 'profile text' input value
+const inputProfileText = page.querySelector('.form__input[name=profile-text]');
+// define 'place name' input value
+const inputPlaceName = formAddPlace.querySelector(
+  '.form__input[name=place-name]'
+);
+// define 'place text' input value
+const inputPlaceImage = formAddPlace.querySelector(
+  '.form__input[name=place-image]'
+);
 
 // FUNCTIONS
 function openPopup(popup) {
@@ -81,18 +86,18 @@ function openPopupAddPlace() {
   openPopup(popupAddPlace);
 
   // clear old name and text in input fields
-  inputPlaceName.value = "";
-  inputPlaceImage.value = "";
+  inputPlaceName.value = '';
+  inputPlaceImage.value = '';
 }
 
 function openPopupEnlargeImage(event) {
   const imageLink = event.target.src;
   const placeName = event.target
-    .closest(".card")
-    .querySelector(".card__name").textContent;
+    .closest('.card')
+    .querySelector('.card__name').textContent;
 
   popupImage.src = imageLink;
-  popupImage.alt = "Увеличенное изображение места " + placeName;
+  popupImage.alt = 'Увеличенное изображение места ' + placeName;
   popupFigcaption.textContent = placeName;
 
   openPopup(popupEnlargeImage);
@@ -124,8 +129,8 @@ function submitFormAddPlace(event) {
 }
 
 function saveNewProfile(name, text) {
-  profile["name"] = name;
-  profile["text"] = text;
+  profile['name'] = name;
+  profile['text'] = text;
 }
 
 function refreshProfile() {
@@ -139,38 +144,52 @@ function toggleLike(event) {
   btnClasses.toggle(classActiveLikeBtn);
 }
 
-function addPlaceCard(placeName, placeImage, imageAlt = "") {
+function addPlaceCard(placeName, placeImage, imageAlt = '') {
   const newCard = createPlaceCard(placeName, placeImage, imageAlt);
-  cards.prepend(newCard);
+  cardsSection.prepend(newCard);
 }
 
-function createPlaceCard(placeName, placeImage, imageAlt = "") {
+function createPlaceCard(placeName, placeImage, imageAlt = '') {
   // copy of template element
-  const placeCard = templateCard.querySelector(".card").cloneNode(true);
+  const placeCard = templateCard.querySelector('.card').cloneNode(true);
   // elements of card
-  const cardName = placeCard.querySelector(".card__name");
-  const cardImage = placeCard.querySelector(".card__image");
-  const btnRemoveCard = placeCard.querySelector(".card__remove-button");
-  const btnLikeCard = placeCard.querySelector(".card__like-button");
+  const cardName = placeCard.querySelector('.card__name');
+  const cardImage = placeCard.querySelector('.card__image');
+  const btnRemoveCard = placeCard.querySelector('.card__remove-button');
+  const btnLikeCard = placeCard.querySelector('.card__like-button');
   // initialize data place into place card
   cardName.textContent = placeName;
   cardImage.src = placeImage;
-  if (imageAlt === "") {
-    cardImage.alt = "Изображение места " + placeName;
+  if (imageAlt === '') {
+    cardImage.alt = 'Изображение места ' + placeName;
   } else {
     cardImage.alt = imageAlt;
   }
-  // add listeners for new place card
-  btnRemoveCard.addEventListener("click", removeCard);
-  btnLikeCard.addEventListener("click", toggleLike);
-  cardImage.addEventListener("click", openPopupEnlargeImage);
 
   return placeCard;
 }
 
 function removeCard(event) {
-  const currentCard = event.target.closest(".card");
+  const currentCard = event.target.closest('.card');
   currentCard.remove();
+}
+
+function clickHandler(event) {
+  if (event.target.classList.contains(classBtnEditProfile)) {
+    openPopupEditProfile();
+  }
+  if (event.target.classList.contains(classBtnAddPlace)) {
+    openPopupAddPlace();
+  }
+  if (event.target.classList.contains(classCardImage)) {
+    openPopupEnlargeImage(event);
+  }
+  if (event.target.classList.contains(classLikeBtn)) {
+    toggleLike(event);
+  }
+  if (event.target.classList.contains(classRemoveCardBtn)) {
+    removeCard(event);
+  }
 }
 
 // initialize place cards
@@ -179,18 +198,19 @@ initialCards.forEach((place) => {
 });
 
 // add listeners for popups buttons
-btnEditProfile.addEventListener("click", openPopupEditProfile);
-btnClosePopupEditProfile.addEventListener("click", () =>
+profileSection.addEventListener('click', clickHandler);
+cardsSection.addEventListener('click', clickHandler);
+
+btnClosePopupEditProfile.addEventListener('click', () =>
   closePopup(popupEditProfile)
 );
-formEditProfile.addEventListener("submit", submitFormEditProfile);
+formEditProfile.addEventListener('submit', submitFormEditProfile);
 
-btnAddPlace.addEventListener("click", openPopupAddPlace);
-btnClosePopupAddPlace.addEventListener("click", () =>
+btnClosePopupAddPlace.addEventListener('click', () =>
   closePopup(popupAddPlace)
 );
-formAddPlace.addEventListener("submit", submitFormAddPlace);
+formAddPlace.addEventListener('submit', submitFormAddPlace);
 
-btnClosePopupEnlargeImage.addEventListener("click", () =>
+btnClosePopupEnlargeImage.addEventListener('click', () =>
   closePopup(popupEnlargeImage)
 );
