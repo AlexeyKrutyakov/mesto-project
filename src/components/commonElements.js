a0ed8d5341eda8394f23aefc78222f4d128a4bc6
Alexey Krutyakov <coder.arctik@gmail.com>
2023-05-20 13:17:40 +0300

    refactor: commonElements comments

--- src/components/commonElements.js
+++ src/components/commonElements.js
@@ -2,8 +2,10 @@
 export const profileSection = document.querySelector('.profile');
 export const profileName = profileSection.querySelector('.profile__name');
 export const profileAbout = profileSection.querySelector('.profile__text');
+
 // CARDS SECTION
 export const cardsSection = document.querySelector('.cards');
+
 // POPUPS
 // avatar popup
 export const avatarPopupClass = 'popup_type_edit-avatar';
@@ -14,6 +16,7 @@ export const profilePopup = document.querySelector(`.${profilePopupClass}`);
 // place popup
 export const placePopupClass = 'popup_type_add-place';
 export const placePopup = document.querySelector(`.${placePopupClass}`);
+
 // FORMS
 // avatar form
 export const avatarForm = document.forms['edit-avatar'];
@@ -30,12 +33,14 @@ export const placeForm = document.forms['place-info'];
 export const placeInputName = placeForm.elements['place-name-input'];
 // input 'place text'
 export const placeInputImage = placeForm.elements['place-image-input'];
-// popup 'submit' button
+
+// POPUPS 'SUBMIT' BUTTONS
 export const submitBtnClass = 'form__submit';
 export const profileSubmitBnt = profileForm.querySelector(`.${submitBtnClass}`);
 export const avatarSubmitBtn = avatarForm.querySelector(`.${submitBtnClass}`);
 export const placeSubmitBtn = placeForm.querySelector(`.${submitBtnClass}`);
 
+// parameters for validation
 export const validationParameters = {
   formSelector: '.form',
   inputSelector: '.form__input',
