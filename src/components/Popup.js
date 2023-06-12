2858fddbfdd965e9d50591a9954cbd018647df3f
Ivanna Balanyuk <ivannabalanyuk@yandex.ru>
2023-06-12 21:19:46 +0300

    fix: eventLesteners removing

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,6 +3,8 @@ import { page, popupSelectors } from '../components/constants.js';
 export default class Popup {
   constructor(popupSelector) {
     this._popup = page.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
+    this._handleClickClose = this._handleClickClose.bind(this);
   }
 
   _handleEscClose(event) {
@@ -22,13 +24,13 @@ export default class Popup {
 
   open() {
     this._popup.classList.add(popupSelectors.openedPopupClass);
-    page.addEventListener('keydown', this._handleEscClose.bind(this));
-    this._popup.addEventListener('click', this._handleClickClose.bind(this));
+    page.addEventListener('keydown', this._handleEscClose);
+    this._popup.addEventListener('click', this._handleClickClose);
   }
 
   close() {
     this._popup.classList.remove(popupSelectors.openedPopupClass);
-    page.removeEventListener('keydown', this._handleEscClose.bind(this));
-    this._popup.removeEventListener('click', this._handleClickClose.bind(this));
+    page.removeEventListener('keydown', this._handleEscClose);
+    this._popup.removeEventListener('click', this._handleClickClose);
   }
 }
